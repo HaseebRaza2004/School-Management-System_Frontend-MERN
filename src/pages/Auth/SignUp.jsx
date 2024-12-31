15f8effbc0d628712fc9fd19866dcbbc1f4ee8e8
HaseebRaza2004 <156467139+HaseebRaza2004@users.noreply.github.com>
2024-12-31 04:45:23 -0800

    working on login pending

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,7 +1,8 @@
-import Button from "../../components/button"
 
 
 function SignUp() {
+
+
     return (
         <section className="text-gray-600 body-font">
             <div className="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
@@ -16,21 +17,21 @@ function SignUp() {
                     <div className="flex items-center flex-col w-full">
                         <h1 className="mb-10 text-3xl font-bold"> Welcome To <span className="text-cyan-500">AcademiaConnect</span></h1>
                         <button
-                            onClick={()=>(window.location.href = 'http://localhost:4000/auth/google')}
+                            onClick={() => (window.location.href = 'http://localhost:4000/auth/google')}
                             type="button"
                             className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-6 w-full"
                         >
                             Continue With Google
                         </button>
                         <button
-                        onClick={()=>(window.location.href = 'http://localhost:4000/auth/facebook')}
+                            onClick={() => (window.location.href = 'http://localhost:4000/auth/facebook')}
                             type="button"
                             className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-6 w-full"
                         >
                             Continue With Facebook
                         </button>
                         <button
-                        onClick={()=>(window.location.href = 'http://localhost:4000/auth/github')}
+                            onClick={() => (window.location.href = 'http://localhost:4000/auth/github')}
                             type="button"
                             className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-6 w-full"
                         >
@@ -41,6 +42,6 @@ function SignUp() {
             </div>
         </section>
     )
-}
+};
 
-export default SignUp
+export default SignUp;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import HeroSection1 from "../components/heroSection1";
 import HeroSection2 from "../components/heroSection2";
 import HeroSection3 from "../components/heroSection3";
 import Statistics from "../components/statistics";
 import HowItWorks from "../components/stepsHowItWorks";
+import axios from "axios";
 
 function Home() {
+
+    useEffect(() => {
+        axios.get(`http://localhost:4000/api/user`, { withCredentials: true })
+            .then((res) => {
+                console.log("response in frontend from db =>", res.data);
+            })
+            .catch((err) => console.log("error in geting response", err));
+    }, []);
+
     return (
         <div className="container mx-auto">
             <HeroSection1 />
